a391ae64ec98e381d1359120d9ef2a5e7acae4f8
callumhuntington <88731855+callumhuntington@users.noreply.github.com>
2026-05-17 18:53:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'russian-tools-v1';
+const FILES = [
+  '/russian-tools/',
+  '/russian-tools/index.html',
+  '/russian-tools/ch123_quiz.html',
+  '/russian-tools/russian_verb_conjugator.html',
+  '/russian-tools/manifest.json',
+  '/russian-tools/icon.png',
+  '/russian-tools/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
